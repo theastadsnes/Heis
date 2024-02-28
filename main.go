3291b1314497181838d514732ed122098e912a8f
Andrine Terland <andrine.terland@gmail.com>
2024-02-28 14:29:29 +0100

    Testing example from code

--- main.go
+++ main.go
@@ -9,49 +9,20 @@ package main
 import (
 	//"Heis/singleElev/elevio"
 	//"Heis/singleElev/fsm"
-	//"Heis/network/bcast"
-	//"Heis/network/localip"
-	"Heis/network/conn"
+	"Heis/network/bcast"
+	"Heis/network/localip"
+	"Heis/network/peers"
 	"Heis/config"
 	"fmt"
 	"time"
-	"net"
+	"os"
+	"flag"
 )
 
- func sending(channel chan config.LocalElevatorState){
-	fmt.Printf("Hei, kommer inn i func")
-	for {
-		state := config.InitElevState("Elevator1")
-		fmt.Print("Sender state: %+v\n", state)
-
-		channel <- state
-
-		time.Sleep(1 * time.Second) // Juster frekvensen etter behov
-	}
+type HelloMsg struct {
+	Message string
+	Iter    int
 }
-
-// func startUDPServer() {
-//     addr := net.UDPAddr{
-//         Port: 15657,
-//         IP:   net.ParseIP("127.0.0.1"),
-//     }
-//     conn, err := net.ListenUDP("udp", &addr)
-//     if err != nil {
-//         fmt.Println("Failed to start server:", err)
-//         return
-//     }
-//     defer conn.Close()
-
-//     buffer := make([]byte, 1024)
-//     for {
-//         n, _, err := conn.ReadFromUDP(buffer)
-//         if err != nil {
-//             fmt.Println("Failed to read:", err)
-//             continue
-//         }
-//         fmt.Printf("Server received: %s\n", string(buffer[:n]))
-//     }
-// }
 func main() {
 	// Define the number of floors in the building
 	// numFloors := 4
@@ -90,23 +61,63 @@ func main() {
 
 	// fmt.Print(localip.LocalIP())
 
-	conn := conn.DialBroadcastUDP(30000)
-    defer conn.Close()
+	var id string
+	flag.StringVar(&id, "id", "", "id of this peer")
+	flag.Parse()
 
-    for {
-        // Send en enkel tekststreng som en melding
-        message := "Hei fra sender"
-        _, err := conn.WriteTo([]byte(message), &net.UDPAddr{
-            IP:   net.ParseIP("10.22.74.118"),
-            Port: 15657,
-        })
-        if err != nil {
-            fmt.Println("Feil ved sending av melding:", err)
-            return
-        }
-        fmt.Println("Melding sendt:", message)
-        time.Sleep(2 * time.Second)
-    }
+	// ... or alternatively, we can use the local IP address.
+	// (But since we can run multiple programs on the same PC, we also append the
+	//  process ID)
+	if id == "" {
+		localIP, err := localip.LocalIP()
+		if err != nil {
+			fmt.Println(err)
+			localIP = "DISCONNECTED"
+		}
+		id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
+	}
+
+	// We make a channel for receiving updates on the id's of the peers that are
+	//  alive on the network
+	peerUpdateCh := make(chan peers.PeerUpdate)
+	// We can disable/enable the transmitter after it has been started.
+	// This could be used to signal that we are somehow "unavailable".
+	peerTxEnable := make(chan bool)
+	go peers.Transmitter(15647, id, peerTxEnable)
+	go peers.Receiver(15647, peerUpdateCh)
+
+	// We make channels for sending and receiving our custom data types
+	helloTx := make(chan HelloMsg)
+	helloRx := make(chan HelloMsg)
+	// ... and start the transmitter/receiver pair on some port
+	// These functions can take any number of channels! It is also possible to
+	//  start multiple transmitters/receivers on the same port.
+	go bcast.Transmitter(16569, helloTx)
+	go bcast.Receiver(16569, helloRx)
+
+	// The example message. We just send one of these every second.
+	go func() {
+		helloMsg := HelloMsg{"Hello from " + id, 0}
+		for {
+			helloMsg.Iter++
+			helloTx <- helloMsg
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	fmt.Println("Started")
+	for {
+		select {
+		case p := <-peerUpdateCh:
+			fmt.Printf("Peer update:\n")
+			fmt.Printf("  Peers:    %q\n", p.Peers)
+			fmt.Printf("  New:      %q\n", p.New)
+			fmt.Printf("  Lost:     %q\n", p.Lost)
+
+		case a := <-helloRx:
+			fmt.Printf("Received: %#v\n", a)
+		}
+	}
 
 	
 }
