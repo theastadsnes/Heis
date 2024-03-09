227a4c00809656f0ad7c843fcd26c39ed8662068
Martin Huynh <martiniuss.wan@gmail.com>
2024-03-09 12:35:02 +0100

    new chanes in doorlamp

--- statemachines/stateMachines.go
+++ statemachines/stateMachines.go
@@ -46,69 +46,72 @@ func CabOrderFSM(elevator *config.Elevator, orderFloor int, orderButton elevio.B
 
 }
 
-func updateHallOrders(elevator *config.Elevator, orderFloor [config.NumFloors][config.NumButtons-2]bool,  newAssignedOrders *costfunc.AssignmentResults){
+func updateHallOrders(elevator *config.Elevator, orderFloor [config.NumFloors][config.NumButtons - 2]bool, newAssignedOrders *costfunc.AssignmentResults) {
 
-for _, assignments := range (*newAssignedOrders).Assignments {
+	for _, assignments := range (*newAssignedOrders).Assignments {
 		if assignments.ID == elevator.Id {
 			for floor := 0; floor < config.NumFloors; floor++ {
 				if assignments.UpRequests[floor] {
 					elevator.Requests[floor][elevio.BT_HallUp] = 1
 					orderFloor[floor][elevio.BT_HallUp] = true
-					
+
 				} else if !assignments.UpRequests[floor] {
 					elevator.Requests[floor][elevio.BT_HallUp] = 0
-					
+
 				}
 				if assignments.DownRequests[floor] {
 					elevator.Requests[floor][elevio.BT_HallDown] = 1
 					orderFloor[floor][elevio.BT_HallDown] = true
-					
-					
+
 				} else if !assignments.DownRequests[floor] {
 					elevator.Requests[floor][elevio.BT_HallDown] = 0
-					
+
 				}
 			}
 		}
 	}
 }
+
 func HallOrderFSM(elevator *config.Elevator, newAssignedOrders *costfunc.AssignmentResults, doorTimer *time.Timer) {
-	
-	var orderFloor[config.NumFloors][config.NumButtons-2]bool
+
+	var orderFloor [config.NumFloors][config.NumButtons - 2]bool
 	updateHallOrders(elevator, orderFloor, newAssignedOrders)
 
 	for floor := 0; floor < config.NumFloors; floor++ {
-		for button := 0; button <config.NumButtons-2; button++{
-			switch {
-	case elevator.Behaviour == config.EB_DoorOpen:
-		if floor == elevator.Floor {
-			elevio.SetDoorOpenLamp(true)
-			requests.Clear_request_at_floor(elevator)
-			doorTimer.Reset(time.Duration(3) * time.Second)
-		}
-	
-	case elevator.Behaviour == config.EB_Idle:
-		if floor == elevator.Floor {
-			elevio.SetDoorOpenLamp(true)
-			requests.Clear_request_at_floor(elevator)
-			elevator.Behaviour = config.EB_DoorOpen
-			doorTimer.Reset(time.Duration(3) * time.Second)
-		} else {
-			if requests.Requests_above(elevator) {
-				elevator.Dirn = elevio.MD_Up
-				elevio.SetMotorDirection(elevator.Dirn)
-				elevator.Behaviour = config.EB_Moving
-			} else if requests.Requests_below(elevator) {
-				elevator.Dirn = elevio.MD_Down
-				elevio.SetMotorDirection(elevator.Dirn)
-				elevator.Behaviour = config.EB_Moving
+		for button := 0; button < config.NumButtons-2; button++ {
+			if orderFloor[floor][button] {
+
+				switch {
+				case elevator.Behaviour == config.EB_DoorOpen:
+					if floor == elevator.Floor {
+						elevio.SetDoorOpenLamp(true)
+						requests.Clear_request_at_floor(elevator)
+						doorTimer.Reset(time.Duration(3) * time.Second)
+					}
+
+				case elevator.Behaviour == config.EB_Idle:
+					if floor == elevator.Floor {
+						elevio.SetDoorOpenLamp(true)
+						requests.Clear_request_at_floor(elevator)
+						elevator.Behaviour = config.EB_DoorOpen
+						doorTimer.Reset(time.Duration(3) * time.Second)
+					} else {
+						if requests.Requests_above(elevator) {
+							elevator.Dirn = elevio.MD_Up
+							elevio.SetMotorDirection(elevator.Dirn)
+							elevator.Behaviour = config.EB_Moving
+						} else if requests.Requests_below(elevator) {
+							elevator.Dirn = elevio.MD_Down
+							elevio.SetMotorDirection(elevator.Dirn)
+							elevator.Behaviour = config.EB_Moving
+						}
+					}
+
+				}
 			}
 		}
+	}
 
-	}
-		}
-	}
-	
 }
 
 func AssignHallOrders(orderChanTx chan *costfunc.AssignmentResults, ElevatorsMap map[string]config.Elevator) {
@@ -124,17 +127,17 @@ func AssignHallOrders(orderChanTx chan *costfunc.AssignmentResults, ElevatorsMap
 
 func UpdateLights(elevator *config.Elevator, elevatorsMap map[string]config.Elevator) {
 
-	var lights[config.NumFloors][config.NumButtons-2]bool
+	var lights [config.NumFloors][config.NumButtons - 2]bool
 
 	for _, id := range elevatorsMap {
 		for floor := range id.Requests {
 			for button := 0; button < 2; button++ {
 				if id.Requests[floor][button] == 1 {
 					lights[floor][button] = true
-				}  
+				}
 			}
 		}
-		
+
 	}
 	for floor := 0; floor < config.NumFloors; floor++ {
 		for button := 0; button < config.NumButtons-2; button++ {
