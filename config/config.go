4129392481dbf1e3d929ef9e14c7f426418f78ce
Andrine Terland <andrine.terland@gmail.com>
2024-03-11 11:15:11 +0100

    Deleted some unused structs in config

--- config/config.go
+++ config/config.go
@@ -13,26 +13,12 @@ const (
 	NumButtons = 4
 )
 
-var Pair DirnBehaviourPair
-
 type ElevatorBehaviour int
 
 const (
 	EB_Idle ElevatorBehaviour = iota
 	EB_DoorOpen
 	EB_Moving
-)
-
-type DirnBehaviourPair struct {
-	Dirn      elevio.MotorDirection
-	Behaviour ElevatorBehaviour
-}
-
-type ClearRequestVariant int
-
-const (
-	CV_All ClearRequestVariant = iota
-	CV_InDirn
 )
 
 type Elevator struct {
@@ -42,27 +28,6 @@ type Elevator struct {
 	Requests  [][]int
 	Behaviour ElevatorBehaviour
 	IsOnline  bool
-	Config    struct {
-		ClearRequestVariant ClearRequestVariant
-		DoorOpenDurationS   float64
-	}
-}
-
-type RequestState int
-
-const (
-	None      RequestState = 0
-	Order     RequestState = 1
-	Confirmed RequestState = 2
-	Complete  RequestState = 3
-)
-
-type LocalElevatorState struct {
-	ID       string
-	Floor    int
-	Dir      ElevatorBehaviour
-	Requests [][]RequestState
-	Behave   ElevatorBehaviour
 }
 
 func InitElevState(id string) Elevator {
@@ -71,13 +36,11 @@ func InitElevState(id string) Elevator {
 		requests[floor] = make([]int, 4)
 	}
 
-	floor := 0
 	for elevio.GetFloor() == -1 {
 		elevio.SetMotorDirection(elevio.MD_Down)
 	}
-
 	elevio.SetMotorDirection(elevio.MD_Stop)
-	floor = elevio.GetFloor()
+	floor := elevio.GetFloor()
 
 	return Elevator{Id: id,
 		Floor:     floor,
@@ -85,13 +48,6 @@ func InitElevState(id string) Elevator {
 		Requests:  requests,
 		Behaviour: EB_Idle,
 		IsOnline:  true,
-		Config: struct {
-			ClearRequestVariant ClearRequestVariant
-			DoorOpenDurationS   float64
-		}{
-			ClearRequestVariant: 0,
-			DoorOpenDurationS:   3.0,
-		},
 	}
 }
 
